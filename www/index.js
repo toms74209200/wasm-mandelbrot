197470ac147af14c131da2c8f41b6c9223418960
toms74209200 <motomotomato@gmail.com>
2024-04-11 22:33:52 +0900

    :art: Format code

--- www/index.js
+++ www/index.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
 function draw(ctx, canvas_w, canvas_h, data) {
-    let img = new ImageData(new Uint8ClampedArray(data), canvas_w, canvas_h);
-    ctx.putImageData(img, 0, 0);
+  let img = new ImageData(new Uint8ClampedArray(data), canvas_w, canvas_h);
+  ctx.putImageData(img, 0, 0);
 }
 
 const X_MIN = -1.5;
@@ -12,32 +12,42 @@ const Y_MAX = 1.0;
 const MAX_ITER = 64;
 
 console.log("start loding wasm");
-const mandelbrot = import('../mandelbrot/pkg').catch(console.error);
+const mandelbrot = import("../mandelbrot/pkg").catch(console.error);
 
-Promise.all([mandelbrot]).then(async function([
-    { generate_mandelbrot_set, draw_mandelbrot_set }
+Promise.all([mandelbrot]).then(async function ([
+  { generate_mandelbrot_set, draw_mandelbrot_set },
 ]) {
-    console.log("finished loading wasm");
-    const renderBtn = document.getElementById("render");
-    renderBtn .addEventListener('click', ()=> {
-        draw_mandelbrot_set();
-        let wasmResult = null;
-        {
-            const CANVAS_ID = "canvas_hybrid";
-            let canvas = document.getElementById(CANVAS_ID);
-            let context = canvas.getContext('2d');
-            const canvasWidth = canvas.width;
-            const canvasHeight = canvas.height;
+  console.log("finished loading wasm");
+  const renderBtn = document.getElementById("render");
+  renderBtn.addEventListener("click", () => {
+    draw_mandelbrot_set();
+    let wasmResult = null;
+    {
+      const CANVAS_ID = "canvas_hybrid";
+      let canvas = document.getElementById(CANVAS_ID);
+      let context = canvas.getContext("2d");
+      const canvasWidth = canvas.width;
+      const canvasHeight = canvas.height;
 
-            const generateStartTime = Date.now();
-            wasmResult = generate_mandelbrot_set(canvasWidth, canvasHeight, X_MIN, X_MAX, Y_MIN, Y_MAX, MAX_ITER)
-            const generateEndTime = Date.now();
-            const darwStartTime = Date.now();
-            draw(context, canvasWidth, canvasHeight, wasmResult);
-            const darwEndTime = Date.now();
-            const elapsed = generateEndTime - generateStartTime;
-            console.log(`\tgenerate:wasm\tgenerate_elapsed:${elapsed}[ms]`);
-            console.log(`\tdraw: js\tdraw_elapsed:${darwEndTime - darwStartTime}[ms]`);
-        }
-    })
-})
+      const generateStartTime = Date.now();
+      wasmResult = generate_mandelbrot_set(
+        canvasWidth,
+        canvasHeight,
+        X_MIN,
+        X_MAX,
+        Y_MIN,
+        Y_MAX,
+        MAX_ITER
+      );
+      const generateEndTime = Date.now();
+      const darwStartTime = Date.now();
+      draw(context, canvasWidth, canvasHeight, wasmResult);
+      const darwEndTime = Date.now();
+      const elapsed = generateEndTime - generateStartTime;
+      console.log(`\tgenerate:wasm\tgenerate_elapsed:${elapsed}[ms]`);
+      console.log(
+        `\tdraw: js\tdraw_elapsed:${darwEndTime - darwStartTime}[ms]`
+      );
+    }
+  });
+});
